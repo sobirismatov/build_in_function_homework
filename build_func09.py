7ddee33975ea3bedad92236aefe416d1a871bedc
sobirismatov <name@uzcoder96@gmail.com>
2022-10-20 10:48:40 +0500

    pow

--- build_func09.py
+++ build_func09.py
@@ -1,5 +1,5 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func09
-x=4
-y=2
-answer=5*pow(x,2)*pow(y,3)+x*pow(y,2)
+x=2
+y=4
+answer=2*(pow(y,3)+pow(x,2)*y)
