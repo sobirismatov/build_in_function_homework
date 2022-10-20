4da8d4dd3230f1573c62ae19e8e89e03a9733ccb
sobirismatov <name@uzcoder96@gmail.com>
2022-10-20 10:58:34 +0500

    pow round

--- build_func10.py
+++ build_func10.py
@@ -1,5 +1,6 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func10
+from math import pow
 x=8
 y=4
 answer=round((3*pow(y,1/2)+pow(x,2/3)),2)
