49f62fed5dd25fba93e286877707ac5c67677abf
sobirismatov <name@uzcoder96@gmail.com>
2022-10-20 10:29:37 +0500

    natija

--- build_func02.py
+++ build_func02.py
@@ -1,4 +1,3 @@
 # the task condition is placed in the README file
 #ttps://github.com/codeschool43/Build_in_function_homework#build_func02
-answer=round((3*(7/5-9/4)),3)
-print(answer)
+answer=(3*(7/5-9/4))
